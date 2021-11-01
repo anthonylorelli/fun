42764c5a658a9f1e223b21be9a87e634e99b9966
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-10-31 20:20:04 -0700

    Read in json recipe file

--- menu/mmaker.py
+++ menu/mmaker.py
@@ -1,1 +1,11 @@
 # Menu Maker
+import json
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: mmaker.py <recipe file>\n")
+    exit()
+
+f = open(sys.argv[1])
+recipes = json.load(f)
+print(recipes)
